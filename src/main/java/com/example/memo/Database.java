5ce2878cd09b0532d9c2f5e9ec9c95b3295401d7
Michał Rączkiewicz <racz@netcompany.com>
2025-12-31 13:39:24 +0100

    db connection key fetched from key vault

--- src/main/java/com/example/memo/Database.java
+++ src/main/java/com/example/memo/Database.java
@@ -2,6 +2,9 @@ package com.example.memo;
 
 import com.azure.cosmos.CosmosClient;
 import com.azure.cosmos.CosmosClientBuilder;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.security.keyvault.secrets.SecretClient;
+import com.azure.security.keyvault.secrets.SecretClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
@@ -9,18 +12,25 @@ import javax.annotation.PostConstruct;
 @Component
 public class Database {
 
-    @Value("${database.key}")
-    private String key;
+    @Value("${keyvault.endpoint}")
+    private String keyvaultEndpoint;
 
     @Value("${database.endpoint}")
-    private String endpoint;
+    private String databaseEndpoint;
 
     private CosmosClient cosmosClient;
 
     @PostConstruct
     private void connect() {
+        SecretClient secretClient = new SecretClientBuilder()
+                .vaultUrl(keyvaultEndpoint)
+                .credential(new DefaultAzureCredentialBuilder().build())
+                .buildClient();
+
+        String key = secretClient.getSecret("database-key").getValue();
+
         cosmosClient = new CosmosClientBuilder()
-                .endpoint(endpoint)
+                .endpoint(databaseEndpoint)
                 .key(key)
                 .buildClient();
     }
