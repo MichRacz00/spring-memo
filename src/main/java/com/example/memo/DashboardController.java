2623419b601995216524b92cba100da44181cc8f
MichRacz00 <michal.raczkiewicz@hotmail.com>
2026-01-19 14:34:12 +0100

    small fix in .gitignore

--- src/main/java/com/example/memo/DashboardController.java
+++ src/main/java/com/example/memo/DashboardController.java
@@ -8,7 +8,7 @@ public class DashboardController {
 
     @RequestMapping("/")
     public String index() {
-        return "redirect:/dashboard";
+        return "/index.html";
     }
 
     @RequestMapping("/dashboard")
