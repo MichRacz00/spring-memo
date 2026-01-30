0485e948d73f6506fd69192a0c067a3a76cfec76
MichRacz00 <michal.raczkiewicz@hotmail.com>
2026-01-30 14:08:57 +0100

    When note size changes, classes for text input dfields also change. This allows for dynamic resizing

--- src/main/resources/static/dashboard/selection-controls.js
+++ src/main/resources/static/dashboard/selection-controls.js
@@ -5,12 +5,22 @@ const chooseStickyNoteButton = document.getElementById('chooseStickyNote');
 const chooseNotepadButton = document.getElementById('chooseNotepad');
 //const choosePlaceholderButton = document.getElementById('choosePlaceholder');
 
+const noteTitleInput = document.getElementById('noteTitle');
+const noteContentInput = document.getElementById('noteContent');
+
 //const selectorButtons = [chooseStickyNoteButton, chooseNotepadButton, choosePlaceholderButton]
 const selectorButtons = [chooseStickyNoteButton, chooseNotepadButton]
 
 selectorButtons.forEach(button => {
     button.addEventListener("click", () => {
+        noteTitleInput.classList.remove(`size-${currentNoteType}`)
+        noteContentInput.classList.remove(`size-${currentNoteType}`);
+
         currentNoteType = button.dataset.type;
+
+        noteTitleInput.classList.add(`size-${currentNoteType}`);
+        noteContentInput.classList.add(`size-${currentNoteType}`);
+
         updateActiveButton();
     });
 });
